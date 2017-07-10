3e1ad832fb92b52547c1fde354b8e4b6590b26b0
zhengyunping <zhengyunping@weidian.com>
2017-07-10 16:05:26 +0800

    add file

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -68,7 +68,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: './assets/',
+  publicPath: 'react-gallery/assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
